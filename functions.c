cc7f385058593f990bd62f3031546f92840d5242
KDVSS <dhanavenkatasivasai@gmail.com>
2023-08-28 00:49:41 +0200

    First Commit

--- functions.c
+++ functions.c
@@ -1,15 +1,20 @@
 // C program to add two numbers
 #include <stdio.h>
 
- int A =10, B=30, sum;
+ int A =10, B=30,C = 15, sum, multiply;
+ float D;
 
 
 int main()
 {
     sum = A + B;
+    multiply = B*C;
+
   
     // Print the sum
-    printf("Sum of A and B is: %d", sum);
+    printf("Sum of A and B is: %d\n", sum);
+    
+    printf("Multiply of B and C is: %d", multiply);
   
     return 0;
 }
