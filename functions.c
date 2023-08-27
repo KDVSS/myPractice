60dc126efeb6a3d6126e6b7820d2cee581421c1b
KDVSS <dhanavenkatasivasai@gmail.com>
2023-08-28 00:39:50 +0200

    First Commit

---
+++ functions.c
@@ -0,0 +1,15 @@
+// C program to add two numbers
+#include <stdio.h>
+
+ int A =10, B=30, sum;
+
+
+int main()
+{
+    sum = A + B;
+  
+    // Print the sum
+    printf("Sum of A and B is: %d", sum);
+  
+    return 0;
+}
